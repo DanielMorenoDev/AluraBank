33e616560fb11e47d2dc0a97d26898509fee31a9
DanielMorenoDev <daniel@bananadigital.com>
2024-05-30 20:37:36 -0600

    Validacion de Cuil

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-import esUnCuil from "./validar-cuil.js";
+import esUnCUIT from "./validar-cuit.js";
 
-const campoDeFormulario = document.querySelectorAll("[required]");
+const camposDeFormulario = document.querySelectorAll("[required");
 
-campoDeFormulario.forEach((campo)=>{
-campo.addEventListener("blur",()=> verificarCampo(campo))    
+
+camposDeFormulario.forEach((campo) => {
+  campo.addEventListener("blur", () => verificarCampo(campo));
 });
 
-function verificarCampo(campo){
-    if(campo.name=="cuil" && campo.value.length >=11){
-        esUnCuil(campo)
-    }
+function verificarCampo(campo) {
+  if (campo.name == "cuil" && campo.value.length >= 11) {
+    esUnCUIT(campo);
+  }
 }
