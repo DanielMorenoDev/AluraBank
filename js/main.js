abd9f435b1795d4d36757c84096f45b7c29ebcfb
DanielMorenoDev <daniel@bananadigital.com>
2024-06-05 21:15:22 -0600

    ValidityState

--- js/main.js
+++ js/main.js
@@ -3,16 +3,17 @@ import esMayorDeEdad from "./validar-edad.js";
 
 const camposDeFormulario = document.querySelectorAll("[required");
 
-
 camposDeFormulario.forEach((campo) => {
-  campo.addEventListener("blur", () => verificarCampo(campo));
+  campo.addEventListener("blur", () => verificarCampo(campo))
+  campo.addEventListener("invalid", evento => evento.preventDefault())
 });
 
 function verificarCampo(campo) {
   if (campo.name == "cuil" && campo.value.length >= 11) {
     esUnCUIT(campo);
   }
-  if(campo.name == "fecha_nacimiento" && campo.value != ""){
-    esMayorDeEdad(campo)
+  if (campo.name == "fecha_nacimiento" && campo.value != "") {
+    esMayorDeEdad(campo);
   }
+  console.log(campo.validity);
 }
--- js/validar-edad.js
+++ js/validar-edad.js
@@ -1,11 +1,17 @@
-export default function esMayorDeEdad(campo){
-    const fechaDeNacimiento= new Date(campo.value);
-    validarEdad(fechaDeNacimiento)
-    console.log(validarEdad(fechaDeNacimiento));
+export default function esMayorDeEdad(campo) {
+  const fechaNacimiento = new Date(campo.value);
+  //console.log(fechaNacimiento);
+  validarEdad(fechaNacimiento);
+  console.log(validarEdad(fechaNacimiento));
 }
 
-function validarEdad(fecha){
-    const fechaActual= new Date();
-    const fechaMas18= new Date(fecha.getUTCFullYear()+18, fecha.getUTCMonth(), fecha.getUTCDate());
-    return fechaActual >= fechaMas18;
+
+function validarEdad(fecha) {
+  const fechaActual = new Date();
+  const fechaMas18 = new Date(
+    fecha.getUTCFullYear() + 18,
+    fecha.getUTCMonth(),
+    fecha.getUTCDate()
+  );
+  return fechaActual >= fechaMas18;
 }
